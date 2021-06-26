37ec4b66fdcea87ca191edb62b56d011d7fb61fe
Dave Barasona <barasonadave@gmail.com>
2021-06-26 15:05:00 +0800

    first commit

---
+++ application/views/interface/test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/stylesheet/style.css">
+    <title>JD barasona</title>
+</head>
+<body>
+    Are you lost baby gurl?
+</body>
+</html>
